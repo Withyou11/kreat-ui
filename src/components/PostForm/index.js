5520d6ca76b95ea59e32af772986399f524f2bd8
Withyou11 <100573436+Withyou11@users.noreply.github.com>
2023-11-26 10:52:52 +0700

    fix bug

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -18,6 +18,7 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+dayjs.extend(utc);
 function PostForm() {
     const navigate = useNavigate();
     const [inputValue, setInputValue] = useState('');
